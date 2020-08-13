c035d1125d430d895f04d1596cd5e93badd75656
Manasimohan <manasi.mohan.narsapur@gmail.com>
2020-08-13 19:41:45 +0100

    updated example

--- example/real-data.R
+++ example/real-data.R
@@ -8,7 +8,7 @@ data(Alzheimer)
 alpha_var <- 1
 beta_var <- 1
 # G <- as.integer(readline(prompt="Enter the number of groups: "))
-G <- 3
+G <- 9
 
 # LCA fit
 fit <- blca.em(Alzheimer, G)
@@ -16,18 +16,7 @@ Z <- unMAP(MAP(Zscore(Alzheimer, fit)))
 
 Y <- as.matrix(Alzheimer)
 
-res_final <- ICLFit(Z, Y, G, alpha_var, beta_var)
-ICL_val <- res_final$ICL_val_max
-Z <- res_final$Z_max
-ICL_original <- res_final$ICL_original
+# print(ICLGOne(Z, Y, G, alpha_var, beta_var))
 
-#print(ICL_val)
-#print(ncol(Z))
-#print(ICL_original)
-
-
-print(paste('Oringinal ICL value : ', format(ICL_original, digits = 5)))
-print(paste("Incial number of clusters : ", G))
-print(paste("ICL value post processing : ", format(ICL_val, digits = 5)))
-print(paste("Number of clusters post processing : ", ncol(Z)))
-
+res <- ICLFit(Z, Y, G, alpha_var, beta_var)
+ICLsummary()
